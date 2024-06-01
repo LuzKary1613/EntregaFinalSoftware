32d0595703d9b111e48f7e50f2bd7610e7424561
LuzKary1613 <137965602+LuzKary1613@users.noreply.github.com>
2024-05-31 19:48:34 -0600

    clean_data.py

--- grocery_start/clean_data.py
+++ grocery_start/clean_data.py
@@ -2,15 +2,17 @@ import os
 from page import create_app, db
 from page.models import Item
 
+# Función para limpiar la base de datos
 def clean_database():
-    app = create_app()
-    with app.app_context():
-        if db.engine.dialect.has_table(db.engine.connect(), 'items'):
-            db.session.query(Item).delete()
-            db.session.commit()
-            print('Table "items" cleaned.')
+    app = create_app()  # Crea una instancia de la aplicación Flask
+    with app.app_context():  # Crea un contexto de aplicación para interactuar con la base de datos
+        if db.engine.dialect.has_table(db.engine.connect(), 'items'):  # Verifica si la tabla 'items' existe
+            db.session.query(Item).delete()  # Elimina todos los registros de la tabla 'items'
+            db.session.commit()  # Confirma los cambios en la base de datos
+            print('Table "items" cleaned.')  # Imprime un mensaje indicando que la tabla fue limpiada
         else:
-            print('Table "items" does not exist.')
+            print('Table "items" does not exist.')  # Imprime un mensaje si la tabla no existe
 
+# Ejecuta la función de limpieza de la base de datos si el script se ejecuta directamente
 if __name__ == '__main__':
     clean_database()
