abf0d57e657cf277b40ec73997f773d659d65b4b
LuzKary1613 <137965602+LuzKary1613@users.noreply.github.com>
2024-05-31 19:49:50 -0600

    main.py

--- grocery_start/main.py
+++ grocery_start/main.py
@@ -1,8 +1,9 @@
 from page import create_app
 
+# Crea una instancia de la aplicación Flask utilizando la función create_app
 app = create_app()
 
+# Ejecuta la aplicación Flask si este script se ejecuta directamente
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=True)  # Ejecuta la aplicación en modo de depuración
 
