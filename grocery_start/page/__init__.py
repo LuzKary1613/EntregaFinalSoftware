ae5764a2aa6f41c8ba875560c0a8e00db28edebc
LuzKary1613 <137965602+LuzKary1613@users.noreply.github.com>
2024-05-31 19:45:29 -0600

    __init__.py

--- grocery_start/page/__init__.py
+++ grocery_start/page/__init__.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db = SQLAlchemy()  # Inicializa la extensión SQLAlchemy
 
 def create_app(config_class='config.Config', init_db=False):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-    db.init_app(app)
+    app = Flask(__name__)  # Crea una instancia de la aplicación Flask
+    app.config.from_object(config_class)  # Carga la configuración desde un objeto de configuración
+    db.init_app(app)  # Inicializa la aplicación Flask con la extensión SQLAlchemy
 
     if init_db:
         with app.app_context():
             db.create_all()  # Solo crea las tablas si se especifica
 
     from . import routes  # Asegúrate de que esta importación no cause problemas
-    app.register_blueprint(routes.main)
-    return app
-
+    app.register_blueprint(routes.main)  # Registra el blueprint de las rutas en la aplicación
+    return app  # Devuelve la instancia de la aplicación Flask
