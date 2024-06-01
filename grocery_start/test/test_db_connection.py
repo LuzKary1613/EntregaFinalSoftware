75141bca076dcb12a75293a70647bbce4c354205
LuzKary1613 <137965602+LuzKary1613@users.noreply.github.com>
2024-05-31 19:47:41 -0600

    test_db_connection.py

--- grocery_start/test/test_db_connection.py
+++ grocery_start/test/test_db_connection.py
@@ -1,7 +1,9 @@
 import psycopg2
 
+# Función para probar la conexión a la base de datos
 def test_connection():
     try:
+        # Establece la conexión con la base de datos PostgreSQL
         conn = psycopg2.connect(
             dbname="grocery_db",
             user="postgres",
@@ -10,14 +12,15 @@ def test_connection():
             port="5432",
             options="-c client_encoding=UTF8"
         )
-        cur = conn.cursor()
-        cur.execute("SELECT 1;")
-        print("Connection successful")
-        cur.close()
-        conn.close()
+        cur = conn.cursor()  # Crea un cursor para ejecutar consultas
+        cur.execute("SELECT 1;")  # Ejecuta una consulta simple para probar la conexión
+        print("Connection successful")  # Imprime un mensaje si la conexión es exitosa
+        cur.close()  # Cierra el cursor
+        conn.close()  # Cierra la conexión
     except Exception as e:
+        # Maneja cualquier excepción que ocurra y muestra un mensaje de error
         print(f"Error connecting to the database: {e}")
 
+# Ejecuta la función de prueba de conexión si el script se ejecuta directamente
 if __name__ == '__main__':
     test_connection()
-
